ba58657855686b8c8023d9e17808f7b54ad3d728
junehee <dev.junehee@gmail.com>
2023-09-02 00:06:13 +0900

    [PGM] 나머지 구하기 / Level 0 / 1분

---
+++ programmers/Level 0/중앙값구하기.js
@@ -0,0 +1,12 @@
+// 변수 선언
+function solution(array) {
+  let newArr = array.sort((a, b) => a - b);
+  return newArr[Math.floor(newArr.length / 2)];
+}
+
+// 변수 선언 생략
+function solution(array) {
+  return array.sort((a, b) => a - b)[Math.floor(array.length / 2)];
+}
+
+// sort() 메서드는 원본을 변경하므로 굳이 변수 선언을 하지 않아도 된다.
