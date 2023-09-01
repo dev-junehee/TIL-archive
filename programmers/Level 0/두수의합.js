4e12e18e57861cd55d5fe01f591438ac0494602e
junehee <dev.junehee@gmail.com>
2023-09-01 23:47:51 +0900

    [PGM] 두 수의 합 / Level 0 / 1분

---
+++ programmers/Level 0/두수의합.js
@@ -0,0 +1,13 @@
+// 변수 선언
+function solution(num1, num2) {
+  let answer = num1 + num2;
+  return answer;
+}
+
+// 변수 선언 생략
+function solution(num1, num2) {
+  return num1 + num2;
+}
+
+// 화살표 함수
+const solution = (num1, num2) => num1 + num2;
