e3eb975e73d7b774fc18a6edcf1d780d92f28fb1
junehee <dev.junehee@gmail.com>
2023-09-01 23:57:04 +0900

    [PGM] 숫자 비교하기 / Level 0 / 5분

---
+++ programmers/Level 0/숫자비교하기.js
@@ -0,0 +1,13 @@
+// if문 활용
+function solution(num1, num2) {
+  if (num1 === num2) {
+    return 1;
+  } else {
+    return -1;
+  }
+}
+
+// 삼항연산자 활용
+function solution(num1, num2) {
+  return num1 === num2 ? 1 : -1;
+}
