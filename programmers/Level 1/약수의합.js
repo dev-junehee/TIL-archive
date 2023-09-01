457866e3daaf5668ee232010dfbbe62801978468
junehee <dev.junehee@gmail.com>
2023-09-02 01:42:59 +0900

    [PGM] 약수의 합 / Level 1 / 30분

---
+++ programmers/Level 1/약수의합.js
@@ -0,0 +1,13 @@
+function solution(num) {
+  const divisors = [];
+
+  for (let i = 1; i <= num / 2; i++) {
+    if (num % i === 0) {
+      divisors.push(i).push(num);
+    }
+  }
+
+  const sum = divisors.reduce((a, b) => a + b);
+
+  return sum;
+}
