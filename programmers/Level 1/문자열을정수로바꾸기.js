8ff79586c503eedac653ca91fdf56ffb1ee46eea
junehee <dev.junehee@gmail.com>
2023-09-03 14:21:08 +0900

    [PGM] 문자열을 정수로 바꾸기 / Level 1 / 10초

---
+++ programmers/Level 1/문자열을정수로바꾸기.js
@@ -0,0 +1,20 @@
+function solution(s) {
+  let answer = 0 + Number(s);
+  return answer;
+}
+
+function solution(s) {
+  return s * 1;
+}
+
+function solution(s) {
+  return s / 1;
+}
+
+function solution(s) {
+  return s - 0;
+}
+
+// 참고자료
+// 자바스크립트에서 문자열을 숫자로 변환하는 방법
+// https://www.freecodecamp.org/korean/news/how-to-convert-a-string-to-a-number-in-javascript/
