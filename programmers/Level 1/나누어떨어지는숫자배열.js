ae93a700e09f027efa958d3a35b26562684b4acf
junehee <dev.junehee@gmail.com>
2023-09-03 14:29:13 +0900

    [PGM] 나누어 떨어지는 숫자 배열 / Level 1 / 30분

---
+++ programmers/Level 1/나누어떨어지는숫자배열.js
@@ -0,0 +1,28 @@
+function solution(arr, divisor) {
+  let answer = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % divisor === 0) {
+      answer.push(arr[i]);
+    }
+  }
+
+  if (answer.length === 0) {
+    answer.push(-1);
+  } else {
+    answer.sort((a, b) => a - b);
+  }
+
+  return answer;
+}
+
+// short coding
+function solution(arr, divisor) {
+  let answer = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % divisor === 0) answer.push(arr[i]);
+  }
+
+  return answer.length === 0 ? [-1] : answer.sort((a, b) => a - b);
+}
