fd56c7cdb8eafd68eb1bff53973f22ac8a164ed7
JuneHee <dev.junehee@gmail.com>
2023-12-18 14:21:45 +0900

    [Bronze III] Title: 곱셈, Time: 124 ms, Memory: 9588 KB -BaekjoonHub

--- 백준/Bronze/2588. 곱셈/곱셈.js
+++ 백준/Bronze/2588. 곱셈/곱셈.js
@@ -4,7 +4,7 @@ let input = fs.readFileSync("dev/stdin").toString().split("\n");
 const [a, b] = input;
 const [b_1, b_2, b_3] = b.split("");
 
-console.log(a * b_3);
-console.log(a * b_2);
-console.log(a * b_1);
-console.log(a * b);
+console.log(Number(a) * Number(b_3));
+console.log(Number(a) * Number(b_2));
+console.log(Number(a) * Number(b_1));
+console.log(Number(a) * Number(b));
