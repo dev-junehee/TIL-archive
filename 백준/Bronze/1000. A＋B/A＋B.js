7c8b6f55513c7aead2ac184fcd8deefadd3fdb6c
JuneHee <dev.junehee@gmail.com>
2023-12-18 13:40:26 +0900

    Update A＋B.js

--- 백준/Bronze/1000. A＋B/A＋B.js
+++ 백준/Bronze/1000. A＋B/A＋B.js
@@ -1,3 +1,4 @@
+// ~~ 사용 (시간 단축)
 let fs = require('fs');
 let input = fs.readFileSync('/dev/stdin').toString().split('\n');
 
@@ -5,5 +6,16 @@ let line = input[0].split(' ');
 
 let A = ~~(line[0]);
 let B = ~~(line[1]);
+
+console.log(A + B);
+
+// parseInt 사용
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().split("\n");
+
+const [a, b] = input[0].split(" ");
+
+const A = parseInt(a, 10);
+const B = parseInt(b, 10);
 
 console.log(A + B);
