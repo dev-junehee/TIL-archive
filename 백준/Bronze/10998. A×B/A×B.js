e6024323a7e9f1aaf0c5216c3dacccaf13ed3607
JuneHee <dev.junehee@gmail.com>
2023-12-18 13:52:54 +0900

    Update A×B.js

--- 백준/Bronze/10998. A×B/A×B.js
+++ 백준/Bronze/10998. A×B/A×B.js
@@ -1,3 +1,4 @@
+// 명시적 형변환
 let fs = require("fs");
 let input = fs.readFileSync("dev/stdin").toString().split("\n");
 
@@ -5,3 +6,12 @@ const A = ~~input[0].split(" ")[0];
 const B = ~~input[0].split(" ")[1];
 
 console.log(A * B);
+
+// 구조분해할당 후 암묵적 형변환 사용
+let fs = require("fs");
+let input = fs.readFileSync("example1.txt").toString().split("\n");
+
+const [A, B] = input[0].split(" ");
+
+console.log(A * B);
+
