0ab845196c0683ecf1831680311366b838f371da
JuneHee <dev.junehee@gmail.com>
2023-12-18 13:39:09 +0900

    [Bronze V] Title: A+B, Time: 124 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.js
@@ -0,0 +1,9 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+let line = input[0].split(' ');
+
+let A = ~~(line[0]);
+let B = ~~(line[1]);
+
+console.log(A + B);
