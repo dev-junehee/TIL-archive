03ca1af0498607d93fd1f5db497bd364f43f4ff6
JuneHee <dev.junehee@gmail.com>
2024-01-03 10:17:43 +0900

    [level unrated] Title: 문자열 곱하기, Time: 0.02 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181940. 문자열 곱하기/문자열 곱하기.js
+++ 프로그래머스/unrated/181940. 문자열 곱하기/문자열 곱하기.js
@@ -1,11 +1,3 @@
 function solution(my_string, k) {
-    let answer = '';
-    let repeat = 1;
-    
-    while (repeat <= k) {
-        answer += my_string;
-        repeat += 1;
-    }
-    
-    return answer;
+    return my_string.repeat(k);
 }
