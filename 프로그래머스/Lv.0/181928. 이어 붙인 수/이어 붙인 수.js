c1a3995e1f3923f8fbb3cbe343d6be94642621f1
JuneHee <dev.junehee@gmail.com>
2024-01-11 21:39:27 +0900

    [level Lv.0] Title: 이어 붙인 수, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/181928. 이어 붙인 수/이어 붙인 수.js
@@ -0,0 +1,11 @@
+function solution(num_list) {
+    let odd = '';
+    let even = '';
+    
+    num_list.map(num => {
+        if (num % 2 === 0) even += num;
+        else odd += num;
+    })
+    
+    return Number(odd) + Number(even);
+}
