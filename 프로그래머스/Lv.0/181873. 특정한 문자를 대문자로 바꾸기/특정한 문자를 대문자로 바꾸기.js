96f5f0964ebd92b84032c37a52dbc97743061385
JuneHee <dev.junehee@gmail.com>
2024-01-11 21:33:02 +0900

    [level Lv.0] Title: 특정한 문자를 대문자로 바꾸기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/Lv.0/181873. 특정한 문자를 대문자로 바꾸기/특정한 문자를 대문자로 바꾸기.js
+++ 프로그래머스/Lv.0/181873. 특정한 문자를 대문자로 바꾸기/특정한 문자를 대문자로 바꾸기.js
@@ -1,10 +1,14 @@
+// function solution(my_string, alp) {
+//     let answer = '';
+    
+//     [...my_string].map(el => {
+//         if (el === alp) answer += el.toUpperCase();
+//         else answer += el;
+//     })
+    
+//     return answer;
+// }
+
 function solution(my_string, alp) {
-    let answer = '';
-    
-    [...my_string].map(el => {
-        if (el === alp) answer += el.toUpperCase();
-        else answer += el;
-    })
-    
-    return answer;
+    return my_string.replaceAll(alp, alp.toUpperCase());
 }
