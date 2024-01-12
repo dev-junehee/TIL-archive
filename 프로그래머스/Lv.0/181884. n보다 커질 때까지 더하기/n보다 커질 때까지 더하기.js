f212aefadb1868783108c29318c005b57c610c1b
JuneHee <dev.junehee@gmail.com>
2024-01-12 22:46:28 +0900

    [level Lv.0] Title: n보다 커질 때까지 더하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.js
@@ -0,0 +1,11 @@
+function solution(numbers, n) {
+    let answer = 0;
+    let i = 0;
+    
+    while (answer <= n) {
+        answer += numbers[i];
+        i++;
+    }
+    
+    return answer;
+}
