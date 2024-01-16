4644eab08400877d8b5750739923a0bc81793522
JuneHee <dev.junehee@gmail.com>
2024-01-16 10:47:35 +0900

    [level Lv.0] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.js
@@ -0,0 +1,16 @@
+function solution(n) {
+    let answer = 0;
+    
+    if (n % 2 === 0) {
+        for (let i = 2; i <= n; i += 2) {
+            answer += i * i;
+        }
+        return answer;
+    } else {
+        for (let i = 1; i <= n; i += 2) {
+            answer += i;
+        }
+        return answer;
+    }
+    
+}
