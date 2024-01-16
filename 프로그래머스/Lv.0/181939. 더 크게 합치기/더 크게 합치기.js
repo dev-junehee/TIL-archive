13e2dec2fa997881e615762f8605ae4cd2b8dc49
JuneHee <dev.junehee@gmail.com>
2024-01-16 10:37:19 +0900

    [level Lv.0] Title: 더 크게 합치기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/Lv.0/181939. 더 크게 합치기/더 크게 합치기.js
+++ 프로그래머스/Lv.0/181939. 더 크게 합치기/더 크게 합치기.js
@@ -2,5 +2,5 @@ function solution(a, b) {
     let AB = parseInt(String(a) + String(b), 10);
     let BA = parseInt(String(b) + String(a), 10);
     
-    return AB - BA > 0 ? AB : BA;
+    return Math.max(AB, BA)
 }
