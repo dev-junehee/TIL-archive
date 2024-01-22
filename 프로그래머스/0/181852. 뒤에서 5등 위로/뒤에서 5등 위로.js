6fcabccb7ab7bf49e010ed20bb83b3f7ffd512cc
JuneHee <dev.junehee@gmail.com>
2024-01-22 21:07:03 +0900

    [level 0] Title: 뒤에서 5등 위로, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.js
+++ 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.js
@@ -1,11 +1,3 @@
 function solution(num_list) {
-    var answer = [];
-    
-    let new_list = num_list.sort((a, b) => a - b);
-    
-    for (let i = 5; i < new_list.length; i++) {
-        answer.push(new_list[i])
-    }
-    
-    return answer;
+    return [...num_list].sort((a, b) => a - b).slice(5);
 }
