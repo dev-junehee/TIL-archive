0eae6375d964a4ac9ea5ff6ae4a35947905788af
JuneHee <dev.junehee@gmail.com>
2024-01-23 11:32:58 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.js
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.js
@@ -1,4 +1,8 @@
+// function solution(my_string, suffix) {
+//     let str = my_string.split('').reverse().slice(0, suffix.length).reverse().join('');
+//     return str === suffix ? 1 : 0;
+// }
+
 function solution(my_string, suffix) {
-    let str = my_string.split('').reverse().slice(0, suffix.length).reverse().join('');
-    return str === suffix ? 1 : 0;
+    return my_string.endsWith(suffix) ? 1 : 0;
 }
