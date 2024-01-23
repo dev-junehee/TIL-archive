ee60b4b29ae96720010c63cffd404d51de92c3c3
JuneHee <dev.junehee@gmail.com>
2024-01-23 11:08:37 +0900

    [level 0] Title: 첫 번째로 나오는 음수, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.js
+++ 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.js
@@ -1,10 +1,3 @@
 function solution(num_list) {
-    let idx = 0;
-    
-    for (let i = 0; i < num_list.length; i++) {
-        if (num_list[i] < 0) return idx;
-        idx++;
-    }
-    
-    return idx === num_list.length ? -1 : idx;
+    return num_list.findIndex(num => num < 0);
 }
