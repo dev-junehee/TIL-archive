4ea6d991cab06e741518552c7bc8a7f98d51bfa7
JuneHee <dev.junehee@gmail.com>
2024-01-23 23:36:28 +0900

    [level 0] Title: 할 일 목록, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181885. 할 일 목록/할 일 목록.js
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.js
@@ -1,9 +1,13 @@
+// function solution(todo_list, finished) {
+//     let answer = [];
+    
+//     for (let i = 0; i < finished.length; i++) {
+//         if (!finished[i]) answer.push(todo_list[i])
+//     }
+    
+//     return answer;
+// }
+
 function solution(todo_list, finished) {
-    let answer = [];
-    
-    for (let i = 0; i < finished.length; i++) {
-        if (!finished[i]) answer.push(todo_list[i])
-    }
-    
-    return answer;
+    return todo_list.filter((todo, i) => !finished[i]);
 }
