52e9bd5dcb1b78bbbdcbe7d3d7cc00be859c2367
JuneHee <dev.junehee@gmail.com>
2024-01-24 10:18:20 +0900

    [level 0] Title: A 강조하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181874. A 강조하기/A 강조하기.js
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.js
@@ -1,5 +1,9 @@
+// function solution(myString) {
+//     return [...myString]
+//         .map(alp => alp === 'A' || alp === 'a' ? alp.toUpperCase() : alp.toLowerCase())
+//         .join('');
+// }
+
 function solution(myString) {
-    return [...myString]
-        .map(alp => alp === 'A' || alp === 'a' ? alp.toUpperCase() : alp.toLowerCase())
-        .join('');
+    return myString.toLowerCase().replaceAll('a', 'A');
 }
