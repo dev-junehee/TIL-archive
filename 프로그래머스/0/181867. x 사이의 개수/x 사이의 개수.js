8f2cf6467c0cfd793cd5f1f8af0f9ba90ad782d0
JuneHee <dev.junehee@gmail.com>
2024-01-27 21:34:01 +0900

    [level 0] Title: x 사이의 개수, Time: 488.18 ms, Memory: 68 MB -BaekjoonHub

--- 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.js
+++ 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.js
@@ -1,3 +1,7 @@
+// function solution(myString) {
+//     return myString.split("x").map(char => char.length);
+// }
+
 function solution(myString) {
-    return myString.split("x").map(char => char.length);
+    return myString.split("x").reduce((acc, cur) => [...acc, cur.length], []);
 }
