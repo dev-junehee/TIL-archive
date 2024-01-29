ba44b5ebbfe8563a2a2bc109068f36e4b86bbf62
JuneHee <dev.junehee@gmail.com>
2024-01-29 20:16:46 +0900

    [level 0] Title: 접미사 배열, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181909. 접미사 배열/README.md
+++ 프로그래머스/0/181909. 접미사 배열/README.md
@@ -1,10 +1,10 @@
 # [level 0] 접미사 배열 - 181909 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181909) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181909?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.09 ms
+메모리: 33.5 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 1일 20:12:56
+2024년 1월 1일 20:16:45
 
 ### 문제 설명
 
--- 프로그래머스/0/181909. 접미사 배열/접미사 배열.js
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.js
@@ -1,13 +1,3 @@
 function solution(my_string) {
-    let suffix = [];
-    
-    my_string = my_string.split('').reverse();
-    let idx = 0;
-    
-    while (idx <= my_string.length) {
-        suffix.push(my_string.slice(0, idx).reverse().join(''))
-        idx += 1;
-    }
-
-    return suffix.filter(str => str.length > 0).sort();
+  return Array.from(my_string).map((_, i) => my_string.substring(i)).sort();
 }
