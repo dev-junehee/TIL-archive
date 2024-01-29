e78ae555466f72c21b1cef1652d75d68ba393156
JuneHee <dev.junehee@gmail.com>
2024-01-29 20:04:06 +0900

    [level 0] Title: 배열 만들기 3, Time: 0.48 ms, Memory: 37.6 MB -BaekjoonHub

--- 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.js
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.js
@@ -1,10 +1,15 @@
+// function solution(arr, intervals) {
+//     let answer = [];
+    
+//     const [[a1, b1], [a2, b2]] = intervals;
+    
+//     for (let i = a1; i <= b1; i++) answer.push(arr[i]);
+//     for (let j = a2; j <= b2; j++) answer.push(arr[j]);
+    
+//     return answer;
+// }
+
 function solution(arr, intervals) {
-    var answer = [];
-    
     const [[a1, b1], [a2, b2]] = intervals;
-    
-    for (let i = a1; i <= b1; i++) answer.push(arr[i]);
-    for (let j = a2; j <= b2; j++) answer.push(arr[j]);
-    
-    return answer;
+    return [...arr.slice(a1, b1 + 1), ...arr.slice(a2, b2 + 1)];
 }
