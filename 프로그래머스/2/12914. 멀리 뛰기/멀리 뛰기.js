33378c425cdc61b0ec42a7af72aa59f576ecea7e
JuneHee <dev.junehee@gmail.com>
2024-04-10 12:40:34 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.55 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -1,11 +1,14 @@
 function solution(n) {
-    if (n <= 2) return n;
+    return recursion(n);
+}
+
+function recursion(n, stack = {}) {
+    if (n === 1) return 1;
+    if (n === 2) return 2;
+    if (stack[n]) return stack[n];
     
-    let arr = [1, 1];
+    const count = recursion(n - 1, stack) + recursion(n - 2, stack);
+    stack[n] = count; 
     
-    for (let i = 2; i <= n; i += 1) {
-        arr.push((arr[i - 1] + arr[i - 2]) % 1234567);
-    }
-    
-    return arr[n]
+    return count % 1234567;
 }
