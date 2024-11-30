6d18519f9cc22add093a8b4689f5ff93c63548f1
JuneHee <dev.junehee@gmail.com>
2024-11-30 21:29:03 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -1,10 +1,4 @@
-// function solution(s) {
-//     let min = Math.min(...s.split(" "));
-//     let max = Math.max(...s.split(" "));
-//     return `${min} ${max}`;
-// }
-
 function solution(s) {
-    const sortS = s.split(" ").sort((a, b) => a - b);
-    return `${sortS[0]} ${sortS[sortS.length - 1]}`
+    let arrS = s.split(" ");
+    return `${Math.min(...arrS)} ${Math.max(...arrS)}`;
 }
