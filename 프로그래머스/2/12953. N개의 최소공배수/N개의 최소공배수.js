6914f2f629ac4160e51d51a77b432f80ca76ef66
JuneHee <dev.junehee@gmail.com>
2024-11-30 21:41:31 +0900

    [level 2] Title: N개의 최소공배수, Time: 80.52 ms, Memory: 36.5 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.js
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.js
@@ -5,10 +5,13 @@ function solution(arr) {
 
 function getLCM (num1, num2) {
     let lcm;
-    if (num1 > num2) lcm = num1;
+    
+    // 둘 중의 큰 값을 임시 최소공배수로 설정
+    if (num1 > num2) lcm = num1; 
     else lcm = num2;
 
     while (true) {
+        // 최소공배수가 num1, num2로 나누어떨어지면 루프 종료
         if (lcm % num1 == 0 && lcm % num2 == 0) break;
         lcm += 1;
     }
