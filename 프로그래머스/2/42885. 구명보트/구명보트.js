5a8e52998b2e9ac6f5cb9f6ccaed31b84ab3ab0d
JuneHee <dev.junehee@gmail.com>
2024-11-30 22:27:19 +0900

    [level 2] Title: 구명보트, Time: 33.61 ms, Memory: 37.7 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.js
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -1,23 +1,26 @@
 function solution(people, limit) {
-    people.sort((a, b) => a - b);
-    
+    // 몸무게 적은 순으로 정렬
+    people.sort((a, b) => a - b)
+
+    // 보트 개수, 가장 앞의 사람, 가장 뒤의 사람 설정
     let boat = 0;
-    
     let front = 0;
     let back = people.length - 1;
     
     while (front <= back) {
-        let sum = people[front] + people[back];
-        
-        if (sum <= limit) {
-            boat += 1;
-            front++;
+        let sum = people[front] + people[back]
+        if (sum > limit) { // 무거운 사람 한명만 탈 수 있음
+            boat++;
             back--;
-        } else {
-            boat += 1;
+        } else {  // 두명 다 탈 수 있음
+            boat++;
+            front++;
             back--;
         }
     }
-     
+    
+    return boat;
+    
+    
     return boat;
 }
