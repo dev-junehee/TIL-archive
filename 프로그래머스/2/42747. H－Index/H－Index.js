6769d8ac027a3cef5e9321db7159de7e86848ac6
JuneHee <dev.junehee@gmail.com>
2024-12-01 18:44:19 +0900

    [level 2] Title: H-Index, Time: 0.55 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.js
+++ 프로그래머스/2/42747. H－Index/H－Index.js
@@ -1,12 +1,14 @@
 function solution(citations) {
-    let H_Index = 0;
+    // 최대값을 구하므로 내림차순 정렬
+    // [6, 5, 3, 1, 0]
+    citations.sort((a, b) => b - a)
     
-    citations.sort((a, b) => b - a);
-
+    // H-Index 선언
+    let hIndex = 0;
+    
+    // 가장 많은 인용부터 시작
     for (let i = 0; i < citations.length; i++) {
-        if (citations[i] > i) H_Index++;
-        else break;
+        if (citations[i] > hIndex) hIndex++;
     }
-    
-    return H_Index;
+    return hIndex;
 }
